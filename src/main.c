be414b124363bc12a9d222458cf6d8e654c7c2f3
Vadim Cherenev <sl@anhot.ru>
2019-07-04 14:41:22 +0500

    First walls

--- src/main.c
+++ src/main.c
@@ -32,13 +32,35 @@ const char map[MAP_HEIGHT][MAP_WIDTH] = {
   {0,0,1,1,1,1,0,0}
 };
 
-const char
+const char sine[256] = {   0,   3,   6,   9,  12,  16,  19,  22,  25,  28,  31,  34,  37,  40,  43,  46,
+                          49,  51,  54,  57,  60,  63,  65,  68,  71,  73,  76,  78,  81,  83,  85,  88,
+                          90,  92,  94,  96,  98, 100, 102, 104, 106, 107, 109, 110, 112, 113, 115, 116,
+                         117, 118, 120, 121, 122, 122, 123, 124, 125, 125, 126, 126, 126, 127, 127, 127,
+                         127, 127, 127, 127, 126, 126, 126, 125, 125, 124, 123, 122, 122, 121, 120, 118,
+                         117, 116, 115, 113, 112, 111, 109, 107, 106, 104, 102, 100,  98,  96,  94,  92,
+                          90,  88,  85,  83,  81,  78,  76,  73,  71,  68,  65,  63,  60,  57,  54,  51,
+                          49,  46,  43,  40,  37,  34,  31,  28,  25,  22,  19,  16,  12,   9,   6,   3,
+                           0,  -3,  -6,  -9, -12, -16, -19, -22, -25, -28, -31, -34, -37, -40, -43, -46,
+                         -49, -51, -54, -57, -60, -63, -65, -68, -71, -73, -76, -78, -81, -83, -85, -88,
+                         -90, -92, -94, -96, -98,-100,-102,-104,-106,-107,-109, -11,   0,-112,-113,-115,
+                        -116,-117,-118,-120,-121,-122,-122,-123,-124,-125,-125,-126,-126,-126,-127,-127,
+                        -127,-127,-127,-127,-127,-126,-126,-126,-125,-125,-124,-123,-122,-122,-121,-120,
+                        -118,-117,-116,-115,-113,-112,-111,-109,-107,-106,-104,-102,-100, -98, -96, -94,
+                         -92, -90, -88, -85, -83, -81, -78, -76, -73, -71, -68, -65, -63, -60, -57, -54,
+                         -51, -49, -46, -43, -40, -37, -34, -31, -28, -25, -22, -19, -16, -12,  -9,  -6 };
+
+__sfr __banked __at 0xf7fe joystickKeysPort;
+
+long int playerX;
+long int playerY;
 
 void fillScreen(char pattern);
 void fillScreenAttrs(char pattern);
 void drawColumnOfAttrs(unsigned char left, unsigned char top, unsigned char bottom);
 void renderWalls();
 void calculateWalls();
+long int traceRay(long int myX, long int myY, unsigned char angle);
+char getMapAt(long int x, long int y);
 
 void main()
 {
@@ -46,9 +68,50 @@ void main()
     screenAttrs = (char *)ATTRS_BUFFER_START;
 
     fillScreen((char)0xaa);
-    calculateWalls();
-    renderWalls();
+
+    playerX = (4 << 8);
+    playerY = (4 << 8);
+
+
+    while (1) {
+
+        if (joystickKeysPort & 0b00001000) {
+            playerX += 100;
+        }
+        if (joystickKeysPort & 0b00000100) {
+            playerX -= 100;
+        }
+        if (joystickKeysPort & 0b00000010) {
+            playerY += 100;
+        }
+        if (joystickKeysPort & 0b00000001) {
+            playerY -= 100;
+        }
+
+        calculateWalls();
+        fillScreenAttrs(0b00111000);
+        renderWalls();
+    }
 }
+
+long int traceRay(long int myX, long int myY, unsigned char angle) {
+    long int x = myX;
+    long int y = myY;
+    long int ray = 0;
+    while (getMapAt(x, y) == 0 && ray < 10000) {
+        x += sine[angle + 64];
+        y += sine[angle];
+        ray += 256;
+    }
+    return ray;
+}
+
+char getMapAt(long int x, long int y){
+    unsigned char mapX = ( x >> 8 ) & 0xff;
+    unsigned char mapY = ( y >> 8 ) & 0xff;
+    return map[mapY][mapX];
+}
+
 
 void renderWalls() {
     int i;
@@ -60,7 +123,7 @@ void renderWalls() {
 void calculateWalls() {
     int i;
     for (i=0; i<SCREEN_CHAR_WIDTH; i++) {
-        wallRenderBuffer[i] = map[0][i];
+        wallRenderBuffer[i] = (traceRay(playerX, playerY, i) >> 8) & 0xff;
     }
 }
 
