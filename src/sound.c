b58a9a3e7b7b3a8c2c31a46c17bd56232624a215
Vadim Cherenev <sl@anhot.ru>
2019-07-22 16:34:18 +0500

    First sound test

--- src/sound.c
+++ src/sound.c
@@ -27,36 +27,9 @@ void initSound() {
 }
 
 void processSound() {
-
-    unsigned char c;
-    unsigned char out ;
-
-    out = 0;
-    while (!(soundPort & 0x0f ^ 0x0f)) {
-        for (c = 0; c < CHANNELS_NUM; c++) {
-
-            if (channels[c].melody != null) {
-                if (channels[c].timeCounter > 0) {
-                    channels[c].timeCounter--;
-                } else {
-                    if (channels[c].melody->time > 0) {
-                        channels[c].tone = channels[c].melody->tone;
-                        channels[c].timeCounter = channels[c].melody->time;
-                        channels[c].melody++;
-                    } else {
-                        initChannel(c);
-                        return;
-                    }
-                }
-            }
-
-            channels[c].freqCounter++;
-            if (channels[c].freqCounter > channels[c].tone) {
-                channels[c].freqCounter = 0;
-                out ^= OUT_BIT_PATTERN;
-            }
-
-        }
+    unsigned char out = 0;
+    for (;;) {
+        out ^= 0b00011001;
         soundPort = out;
     }
 }
